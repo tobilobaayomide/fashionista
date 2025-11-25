69923ab0880a321c6d766f80188e1c3eac85cf9b
Your Name <your.email@example.com>
2025-11-25 09:45:57 +0100

    feat: gallery page button responsiveness

--- src/app/components/SectionNine.tsx
+++ src/app/components/SectionNine.tsx
@@ -1,21 +1,25 @@
 "use client";
 
+import Link from "next/link";
+
 export default function SectionNine() {
   return (
     <section className="w-full h-screen flex items-center justify-center bg-neutral-200 text-black px-6">
       <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row items-center justify-between h-full">
         {/* Left: Call to action and SVG arrow */}
-          <div className="flex flex-col justify-center items-start w-full md:w-1/2 h-full text-left pr-0 md:pr-12 -ml-20 -mt-35">
+        <div className="flex flex-col justify-center items-start w-full md:w-1/2 h-full text-left pr-0 md:pr-12 -ml-20 -mt-35">
           <h2 className="text-3xl md:text-3xl mb-15" style={{ fontFamily: 'Momo Trust Display, sans-serif', letterSpacing: '0.04em' }}>
             Check Out More<br />Pictures Of Our <br />Collections
           </h2>
-          <span className="inline-block align-middle mb-8 group">
-            <svg width="120" height="120" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <circle cx="60" cy="60" r="60" fill="#FF4040" className="transition-colors duration-300 group-hover:fill-white"/>
-              <path d="M35 60H85M85 60C85 60 82.5 57.5 81 56C79.5 54.5 78.5 51.5 78.5 51.5M85 60C85 60 82.25 62.75 81 64C79.5 65.5 78.5 68.5 78.5 68.5" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="transition-colors duration-300 group-hover:stroke-[#FF4040]"/>
-            </svg>
-          </span>
-          <h3 className="text-l -mt-6">GO TO GALLERY</h3>
+          <Link href="/gallery" className="inline-block align-middle mb-8 group" prefetch={false}>
+            <span>
+              <svg width="120" height="120" viewBox="0 0 120 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="60" cy="60" r="60" fill="#FF4040" className="transition-colors duration-300 group-hover:fill-white"/>
+                <path d="M35 60H85M85 60C85 60 82.5 57.5 81 56C79.5 54.5 78.5 51.5 78.5 51.5M85 60C85 60 82.25 62.75 81 64C79.5 65.5 78.5 68.5 78.5 68.5" stroke="white" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" className="transition-colors duration-300 group-hover:stroke-[#FF4040]"/>
+              </svg>
+            </span>
+            <h3 className="text-l mt-3">GO TO GALLERY</h3>
+          </Link>
         </div>
         {/* Right: Editorial image */}
         <div className="relative flex flex-row ">
